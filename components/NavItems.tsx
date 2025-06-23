b70333e1c0d9cc2e3c1b68978b0b7eefb6cdc75e
mitan11 <mitantank00@gmail.com>
2025-06-23 17:15:22 +0530

    Navbar Done

---
+++ components/NavItems.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+
+const navItems = [
+    {
+        label: "Home", href: '/'
+    },
+    {
+        label: "Companions", href: '/companions'
+    },
+    {
+        label: "My Journey", href: '/my-journey'
+    },
+    {
+        label: "Sign In", href: '/sign-in'
+    },
+]
+
+function NavItems() {
+    const pathName = usePathname();
+
+    return (
+        <nav className='flex items-center gap-4'>
+            {navItems.map(({ label, href }, index) => (
+                <Link href={href} key={label} className={cn(pathName === href && 'text-primary font-semibold transition-all')}>{label}</Link>
+            ))}
+        </nav>
+    )
+}
+
+export default NavItems
